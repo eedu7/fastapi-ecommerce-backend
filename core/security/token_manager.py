7304693fa2e72a7eb4c1125b2dd4cbbb61a46a46
eedu7 <mueedahmad067@gmail.com>
2026-01-28 20:21:50 +0500

    Updated the token manager

--- core/security/token_manager.py
+++ core/security/token_manager.py
@@ -1,15 +1,18 @@
 from typing import Literal
 
 from core.redis import init_redis
+from core.settings import settings
 
 
 class TokenManager:
     def __init__(self) -> None:
         self.redis = init_redis()
 
-    async def store_token(
-        self, jti: str, expires_in: int, token_type: Literal["access", "refresh"] = "access"
-    ):
+    async def store_token(self, jti: str, token_type: Literal["access", "refresh"] = "access"):
+        if token_type == "access":
+            expires_in = settings.JWT_ACCESS_TOKEN_EXPIRE_MINUTES * 60  # Minutes to seconds
+        else:
+            expires_in = settings.JWT_REFRESH_TOKEN_EXPIRE_MINUTES * 60
         await self.redis.set(jti, token_type, ex=expires_in)
 
     async def is_token_blacklisted(self, jti: str) -> bool:
